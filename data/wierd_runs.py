f09add252fae9226b9726194d50f8425b37a4791
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-09 19:33:50 -0300

    add weird runs file

---
+++ data/wierd_runs.py
@@ -0,0 +1,2 @@
+# esta hija de puta da negativa
+python run_model_folds.py -f ../../data/months/201404.txt.clean.fold.0 ../../data/months/201404.txt.clean.fold.1 ../../data/months/201404.txt.clean.fold.2 ../../data/months/201404.txt.clean.fold.3 ../../data/months/201404.txt.clean.fold.4 -s 25351 -c entropy -m 6 -i 100 -l 50 -x 150 -j 4
