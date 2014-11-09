622457f4c9e23941b886809dd10da945ccde99b7
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-09 19:34:22 -0300

    update generate.

--- tp/src/generate.py
+++ tp/src/generate.py
@@ -1,10 +1,9 @@
-FILE = '../../data/months/201404.txt.extended'
+import random
 
 if __name__ == '__main__':
 
-    cmd = 'python run_model_beta.py -f %s -t 0.3 -s 489 -c entropy -m %d -i %d -l %d -x 500 -j 10 >> results.csv'
-    for depth in range(3,10):
-        for i in range(100, 3000, 150):
-            for l in range(50, i, 50):
-                print cmd % (FILE, depth, i, l)
+    cmd = 'python run_model_folds.py -f ../../data/data_and_custom_no_rem_tendency/data_and_custom_tendency.csv.clean.fold.0 ../../data/data_and_custom_no_rem_tendency/data_and_custom_tendency.csv.clean.fold.1 ../../data/data_and_custom_no_rem_tendency/data_and_custom_tendency.csv.clean.fold.2 -s %d -c entropy -m 6 -i %d -l %d -x 150 -j 10'
+    for i in range(100, 3000, 150):
+        for l in range(50, i, 50):
+            print cmd % (random.randint(1,10000), i, l)
             
