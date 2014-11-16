10d0edaa8686b33ae9b236ebb1b8edf96ca33d3f
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-16 16:31:03 -0300

    add data_historic scripts

---
+++ tp/src/data_historic_runner.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
+python ../../data/create_folds.py -f ../../data/data_historic/data_historic.csv.clean -k 3
+python run_model_folds.py -f ../../data/data_historic/data_historic.csv.clean.fold.0 ../../data/data_historic/data_historic.csv.clean.fold.1 ../../data/data_historic/data_historic.csv.clean.fold.2 -s 6233 -c entropy -m 6 -i 250 -l 50 -x 150 -j 10 >> ../../data/data_historic/results.csv
