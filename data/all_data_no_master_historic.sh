1aa1441f1c3e1e8bd0112a8269db69a6dbe5f066
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-18 18:42:15 -0300

    fix historic dataset parametrizations.

--- data/all_data_no_master_historic.sh
+++ data/all_data_no_master_historic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./data_no_master_historic.sh 201404.txt abril
-./data_no_master_historic.sh 201403.txt marzo
-./data_no_master_historic.sh 201402.txt febrero
-./data_no_master_historic.sh 201401.txt enero
-./data_no_master_historic.sh 201312.txt diciembre
-./data_no_master_historic.sh 201311.txt noviembre
+./data_no_master_historic.sh 201404 abril
+./data_no_master_historic.sh 201403 marzo
+./data_no_master_historic.sh 201402 febrero
+./data_no_master_historic.sh 201401 enero
+./data_no_master_historic.sh 201312 diciembre
+./data_no_master_historic.sh 201311 noviembre
--- data/sort_split_by_user.sh
+++ data/sort_split_by_user.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo 'sorting by user and date...'
-sort -t, -k1,2 -rn --batch-size=100000 --parallel=6 producto_premium_2014.txt.csv > producto_premium_2014.txt.csv.sorted.user.date
+sort -t, -k1,2 -rn --batch-size=1000 --parallel=6 producto_premium_2014.txt.csv > producto_premium_2014.txt.csv.sorted.user.date
 echo 'creating user files'
 python split_by_user.py
