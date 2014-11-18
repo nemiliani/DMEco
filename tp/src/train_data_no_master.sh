2fd6538bfd5728ffccfe9c14764118bce47d867a
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-18 19:21:07 -0300

    fix

--- tp/src/train_data_no_master.sh
+++ tp/src/train_data_no_master.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python train_model_beta.py -f ../../data/data_no_master/data_no_master.csv.$1.clean -c entropy -m 6 -i 250 -l 50 -u ../../data/data_no_master/data_no_master.model.$1.pickle -x 150 -j 10
+python train_model_beta.py -f ../../data/data_no_master/data_no_master.csv.removed.$1.clean -c entropy -m 6 -i 250 -l 50 -u ../../data/data_no_master/data_no_master.model.$1.pickle -x 150 -j 10
