24ba0e619e0152bcbacbc3e9aba3779a533460ef
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-18 18:52:17 -0300

    all_data historic fix

--- data/all_data_historic.sh
+++ data/all_data_historic.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-./data_historic.sh 201404.txt abril
-./data_historic.sh 201403.txt marzo
-./data_historic.sh 201402.txt febrero
-./data_historic.sh 201401.txt enero
-./data_historic.sh 201312.txt diciembre
-./data_historic.sh 201311.txt noviembre
+./data_historic.sh 201404 abril
+./data_historic.sh 201403 marzo
+./data_historic.sh 201402 febrero
+./data_historic.sh 201401 enero
+./data_historic.sh 201312 diciembre
+./data_historic.sh 201311 noviembre
