7bce77af28ff34c8d0d2bffd64a7e41e218c6ef3
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-18 19:19:25 -0300

    all data no master trainer

---
+++ tp/src/all_train_data_no_master.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./train_data_no_master.sh abril
+./train_data_no_master.sh marzo
+./train_data_no_master.sh febrero
+./train_data_no_master.sh enero
+./train_data_no_master.sh diciembre
+./train_data_no_master.sh noviembre
+
