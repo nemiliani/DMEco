5fef553ccb802edcaea107996a035c1eb7d5bd89
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-19 20:35:26 -0300

    clean data no master historic

---
+++ data/all_clean_month_data_no_master_historic.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+./clean_month_data_no_master_historic.sh 201404
+./clean_month_data_no_master_historic.sh 201403
+./clean_month_data_no_master_historic.sh 201402
+./clean_month_data_no_master_historic.sh 201401
+./clean_month_data_no_master_historic.sh 201312
+./clean_month_data_no_master_historic.sh 201311
