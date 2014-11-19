9d4ce5794f5c69fa7bfc15b04764902e65004ea0
Nicolas Emiliani <n.r.emiliani@gmail.com>
2014-11-19 19:42:51 -0300

    fix matrix data_nothing

--- tp/src/matrix_data_nothing.sh
+++ tp/src/matrix_data_nothing.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
 echo '--abril--' >> ../../data/data_nothing/matrix.txt
-python test_model_beta.py -f ../../data/months/201404.txt.clean -e ../../data/data_nothing/data_nothing.model.abril.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201404.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.abril.pickle >> ../../data/data_nothing/matrix.txt 
 echo '--marzo--' >> ../../data/data_nothing/matrix.txt
-python test_model_beta.py -f ../../data/months/201404.txt.clean -e ../../data/data_nothing/data_nothing.model.marzo.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201403.txt.clean -e ../../data/data_nothing/data_nothing.model.marzo.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201404.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.marzo.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201403.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.marzo.pickle >> ../../data/data_nothing/matrix.txt 
 echo '--febrero--' >> ../../data/data_nothing/matrix.txt
-python test_model_beta.py -f ../../data/months/201404.txt.clean -e ../../data/data_nothing/data_nothing.model.febrero.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201403.txt.clean -e ../../data/data_nothing/data_nothing.model.febrero.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201402.txt.clean -e ../../data/data_nothing/data_nothing.model.febrero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201404.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.febrero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201403.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.febrero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201402.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.febrero.pickle >> ../../data/data_nothing/matrix.txt 
 echo '--enero--' >> ../../data/data_nothing/matrix.txt
-python test_model_beta.py -f ../../data/months/201404.txt.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201403.txt.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201402.txt.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201401.txt.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201404.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201403.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201402.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201401.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.enero.pickle >> ../../data/data_nothing/matrix.txt 
 echo '--diciembre--' >> ../../data/data_nothing/matrix.txt
-python test_model_beta.py -f ../../data/months/201404.txt.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201403.txt.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201402.txt.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201401.txt.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201312.txt.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201404.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201403.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201402.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201401.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201312.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.diciembre.pickle >> ../../data/data_nothing/matrix.txt 
 echo '--noviembre--' >> ../../data/data_nothing/matrix.txt
-python test_model_beta.py -f ../../data/months/201404.txt.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201403.txt.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201402.txt.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201401.txt.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201312.txt.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
-python test_model_beta.py -f ../../data/months/201311.txt.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201404.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201403.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201402.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201401.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201312.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
+python test_model_beta.py -f ../../data/months/201311.txt.data_nothing.clean -e ../../data/data_nothing/data_nothing.model.noviembre.pickle >> ../../data/data_nothing/matrix.txt 
 
