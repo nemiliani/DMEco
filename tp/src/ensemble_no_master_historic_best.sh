a58ab8cb30e9b90cd337cd0e7a082f35f1f8414e
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-23 17:12:33 -0300

    fix

---
+++ tp/src/ensemble_no_master_historic_best.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+python test_ensamble_beta.py -f ../../data/data_no_master_historic/data_no_master_historic.csv.removed.febrero.clean -e \
+../../data/data_no_master_historic/model.850.100.pickle \
+../../data/data_no_master_historic/model.1750.100.pickle \
+../../data/data_no_master_historic/model.1150.100.pickle \
+../../data/data_no_master_historic/model.1900.150.pickle \
+../../data/data_no_master_historic/model.700.200.pickle \
+../../data/data_no_master_historic/model.400.250.pickle \
+../../data/data_no_master_historic/model.250.100.pickle \
+../../data/data_no_master_historic/model.1900.100.pickle \
+../../data/data_no_master_historic/model.550.50.pickle \
+../../data/data_no_master_historic/model.100.50.pickle \
+../../data/data_no_master_historic/model.400.200.pickle \
+../../data/data_no_master_historic/model.700.100.pickle \
+../../data/data_no_master_historic/model.700.50.pickle \
+../../data/data_no_master_historic/model.1150.150.pickle \
+../../data/data_no_master_historic/model.1000.200.pickle \
+../../data/data_no_master_historic/model.1000.100.pickle \
+../../data/data_no_master_historic/model.2500.150.pickle \
+../../data/data_no_master_historic/model.1900.300.pickle \
+../../data/data_no_master_historic/model.2050.350.pickle \
+../../data/data_no_master_historic/model.2500.200.pickle \
+../../data/data_no_master_historic/model.1150.200.pickle 
+
