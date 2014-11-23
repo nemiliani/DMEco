9c584eb011acaa1af947e965f1fc144e3e6f3d76
Nicolas Emiliani <nicolas@motrixi.com>
2014-11-23 16:21:03 -0300

    fix

---
+++ tp/src/ensemble_no_master_historic_all.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+python test_ensamble_beta.py -f ../../data/data_no_master_historic/data_no_master_historic.csv.removed.febrero.clean -e \
+../../data/data_no_master_historic/model.250.50.pickle \
+../../data/data_no_master_historic/model.100.50.pickle \
+../../data/data_no_master_historic/model.550.50.pickle \
+../../data/data_no_master_historic/model.2800.100.pickle \
+../../data/data_no_master_historic/model.250.150.pickle \
+../../data/data_no_master_historic/model.250.100.pickle \
+../../data/data_no_master_historic/model.400.250.pickle \
+../../data/data_no_master_historic/model.250.200.pickle \
+../../data/data_no_master_historic/model.850.100.pickle \
+../../data/data_no_master_historic/model.550.300.pickle \
+../../data/data_no_master_historic/model.700.100.pickle \
+../../data/data_no_master_historic/model.400.150.pickle \
+../../data/data_no_master_historic/model.400.200.pickle \
+../../data/data_no_master_historic/model.1450.150.pickle \
+../../data/data_no_master_historic/model.550.100.pickle \
+../../data/data_no_master_historic/model.2500.150.pickle \
+../../data/data_no_master_historic/model.850.200.pickle \
+../../data/data_no_master_historic/model.1750.100.pickle \
+../../data/data_no_master_historic/model.2050.100.pickle \
+../../data/data_no_master_historic/model.400.100.pickle \
+../../data/data_no_master_historic/model.1000.250.pickle \
+../../data/data_no_master_historic/model.1000.50.pickle \
+../../data/data_no_master_historic/model.1750.150.pickle \
+../../data/data_no_master_historic/model.1000.100.pickle \
+../../data/data_no_master_historic/model.1150.200.pickle \
+../../data/data_no_master_historic/model.1150.150.pickle \
+../../data/data_no_master_historic/model.1750.350.pickle \
+../../data/data_no_master_historic/model.1000.350.pickle \
+../../data/data_no_master_historic/model.1900.150.pickle \
+../../data/data_no_master_historic/model.1750.50.pickle \
+../../data/data_no_master_historic/model.550.250.pickle \
+../../data/data_no_master_historic/model.1600.150.pickle \
+../../data/data_no_master_historic/model.2050.250.pickle \
+../../data/data_no_master_historic/model.1000.200.pickle \
+../../data/data_no_master_historic/model.1600.350.pickle \
+../../data/data_no_master_historic/model.700.150.pickle \
+../../data/data_no_master_historic/model.700.200.pickle \
+../../data/data_no_master_historic/model.1000.150.pickle \
+../../data/data_no_master_historic/model.700.50.pickle \
+../../data/data_no_master_historic/model.2950.100.pickle \
+../../data/data_no_master_historic/model.1900.100.pickle \
+../../data/data_no_master_historic/model.2350.350.pickle \
+../../data/data_no_master_historic/model.1150.100.pickle \
+../../data/data_no_master_historic/model.1750.300.pickle \
+../../data/data_no_master_historic/model.2500.200.pickle \
+../../data/data_no_master_historic/model.400.50.pickle \
+../../data/data_no_master_historic/model.1600.400.pickle \
+../../data/data_no_master_historic/model.2200.350.pickle \
+../../data/data_no_master_historic/model.2950.50.pickle \
+../../data/data_no_master_historic/model.550.200.pickle \
+../../data/data_no_master_historic/model.1900.300.pickle \
+../../data/data_no_master_historic/model.2050.350.pickle
+
